55cd67bf9a86be9780bc5172c024f167c6bca93a
Abhishek Srivastava <cs.abhilive@gmail.com>
2024-07-19 02:13:30 +0530

    More changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useEffect, useState } from "react";
+import { addFavoriteThing, getFavoriteThings } from "./actions";
 
 export default function Home() {
+  const [newFavoriteThing, setNewFavoriteThing] = useState('');
+  const [favoriteThings, setFavoriteThings] = useState<string[]>([]);
+  async function getThings() {
+    const updatedListOfThings = await getFavoriteThings();
+    setFavoriteThings(updatedListOfThings);
+  }
+  useEffect(() => {
+    getThings();
+  }, []);
+  async function addThing() {
+    await addFavoriteThing(newFavoriteThing);
+    setNewFavoriteThing('');
+    await getThings();
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <h1>Hello Martin!</h1>
+      <h2>Luke&apos;s Favorite Things</h2>
+      <input
+        placeholder="New Favorite Thing"
+        value={newFavoriteThing}
+        onChange={(e) => setNewFavoriteThing(e.target.value)}
+        className="border-black border-2"
+      />
+      <button onClick={addThing} className="border-black border-2 hover:text-white hover:bg-black rounded">
+        Add Favorite Thing
+      </button>
+      <ul className="list-disc list-inside">
+        {favoriteThings.map(function (thing) {
+          return <li key={thing}>{thing}</li>
+        })}
+      </ul>
     </main>
   );
 }
